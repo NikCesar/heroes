15e452a552b287000360142146f0b872372ee07b
NikCesar <neonsugar420@gmail.com>
2019-12-21 11:57:56 +0100

    Repository an JPA angepasst

--- camp/src/main/java/ch/bfh/swos/camp/repositories/HeroRepository.java
+++ camp/src/main/java/ch/bfh/swos/camp/repositories/HeroRepository.java
@@ -1,22 +1,20 @@
 package ch.bfh.swos.camp.repositories;
 
 import ch.bfh.swos.camp.model.Hero;
-import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface HeroRepository extends MongoRepository<Hero, String> {
+public interface HeroRepository extends JpaRepository<Hero, String> {
 
     public List<Hero> findAll();
 
     public Optional<Hero> findById(String id);
 
     public List<Hero> findByName(String name);
-
-    public Hero insert(Hero hero);
 
     public void deleteHeroById(String id);
 
