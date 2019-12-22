6a99021f637c5d06d76070e91506cdd108a71912
Matthias Keller <matthias.keller@bfh.ch>
2019-12-22 16:28:07 +0100

    Manually merged DefaultBattleService

--- arena/src/main/java/ch/bfh/swos/arena/service/impl/DefaultBattleService.java
+++ arena/src/main/java/ch/bfh/swos/arena/service/impl/DefaultBattleService.java
@@ -1,9 +1,6 @@
 package ch.bfh.swos.arena.service.impl;
 
-import ch.bfh.swos.arena.model.BattleStats;
-import ch.bfh.swos.arena.model.Hero;
-import ch.bfh.swos.arena.model.HeroFightStats;
-import ch.bfh.swos.arena.model.Party;
+import ch.bfh.swos.arena.model.*;
 import ch.bfh.swos.arena.service.BattleService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,6 +9,8 @@ import org.springframework.stereotype.Service;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Service
 public class DefaultBattleService implements BattleService {
@@ -28,110 +27,107 @@ public class DefaultBattleService implements BattleService {
         List<Hero> heroesHome = new ArrayList<>(partyHome.getMembers());
         List<Hero> heroesAway = new ArrayList<>(partyAway.getMembers());
 
-        LOG.info("Party '"+partyHome.getName()+"' fights against party '"+partyAway.getName()+"'.");
+        LOG.info("Party '" + partyHome.getName() + "' fights with: ");
+        heroesHome.forEach(x -> LOG.info(x.getName()));
+        LOG.info("AGAINST!");
+        LOG.info("Party '" + partyAway.getName() + "' fights with ");
+        heroesAway.forEach(x -> LOG.info(x.getName()));
 
         int duelCount;
         int roundCount = 0;
 
         while (true) {
-
-            List<Hero> losers = new ArrayList<>();
             roundCount++;
 
-            LOG.info("============================================================================================================");
-            LOG.info("Starting round no. "+roundCount);
+            LOG.info("===============================================================================================");
+            LOG.info("Starting round no. " + roundCount);
 
-            // there can be only as much duels as the count of remaining members in the smallest party
-            if (heroesHome.size() > heroesAway.size()) {
-                duelCount = heroesAway.size();
-            } else {
-                duelCount = heroesHome.size();
+            // Get a sorted list by hero initiative
+            List<Hero> attackOrder = Stream.concat(heroesHome.stream(), heroesAway.stream())
+                    .sorted((x, y) -> y.getInitiative() - x.getInitiative())
+                    .collect(Collectors.toList());
+
+            for (int i = 0; i < attackOrder.size(); i++) {
+                Hero attacker = attackOrder.get(0);
+                // Break if all heroes of a team are defeated
+                if (heroesHome.size() == 0 || heroesAway.size() == 0) break;
+                // Let the attacker engage the enemy team
+                if (partyHome.containsHero(attacker)) attack(attacker, heroesAway);
+                else attack(attacker, heroesHome);
+                // Remove attacker from list
+                attackOrder.remove(0);
+                // Remove dead heroes from attackers list and from the teams
+                attackOrder = attackOrder.stream().filter(x -> x.getHp() > 0).collect(Collectors.toList());
+                heroesHome = heroesHome.stream().filter(x -> x.getHp() > 0).collect(Collectors.toList());
+                heroesAway = heroesAway.stream().filter(x -> x.getHp() > 0).collect(Collectors.toList());
             }
 
-            // execute the duels
-            for (int i = 0; i < duelCount; i++) {
-                Hero fighterHome = heroesHome.get(i);
-                Hero fighterAway = heroesAway.get(i);
-                Hero loser = duel(fighterHome, fighterAway);
-                losers.add(loser);
-            }
-
-            // remove the losers from both parties
-            heroesHome.removeAll(losers);
-            heroesAway.removeAll(losers);
-
-            LOG.info("Party '"+partyHome.getName()+"' has "+heroesHome.size()+" members left.");
-            LOG.info("Party '"+partyAway.getName()+"' has "+heroesAway.size()+" members left.");
+            LOG.info("Party '" + partyHome.getName() + "' has " + heroesHome.size() + " members left.");
+            LOG.info("Party '" + partyAway.getName() + "' has " + heroesAway.size() + " members left.");
 
             // check if a party has already lost (no members left), return the winners party name
             if (heroesHome.isEmpty()) {
-                LOG.info("Party '"+partyAway.getName()+"' wins this battle in "+roundCount+" rounds.");
+                LOG.info("Party '" + partyAway.getName() + "' wins this battle in " + roundCount + " rounds.");
                 return new BattleStats(partyAway, partyHome);
             }
 
             if (heroesAway.isEmpty()) {
-                LOG.info("Party '"+partyHome.getName()+"' wins this battle in "+roundCount+" rounds.");
+                LOG.info("Party '" + partyHome.getName() + "' wins this battle in " + roundCount + " rounds.");
                 return new BattleStats(partyHome, partyAway);
             }
         }
     }
 
-    private Hero duel(Hero fighterHome, Hero fighterAway) {
+    private void attack(Hero attacker, List<Hero> heroes) {
 
-        // fighterHome will attack first => attacker and defender will be swapped before first duel
-        Hero attacker = fighterAway;
-        Hero defender = fighterHome;
+        // If attacker is rogue, attack enemy hero at last position, else attack enemy hero at first position
+        Hero defender = attacker.getHeroType() == HeroType.ROGUE ? heroes.get(heroes.size() - 1) : heroes.get(0);
 
-        LOG.info("------------------------------------------------------------------------------------------------------------");
-        LOG.info(attacker.getHeroType().toString()+" "+attacker.getName()+" at position:"+attacker.getPosition()+"(ATK:"+attacker.getAtk()+", DEF:"+attacker.getDef()+", DodgeChance:"+attacker.getDodgeChance()+", INI:"+attacker.getInitiative()+", CritChance:"+attacker.getCritChance()+") with "+f.format(attacker.getHp())+" hp left fights against "+defender.getHeroType().toString()+" "+defender.getName()+" at position:"+defender.getPosition()+"(ATK:"+defender.getAtk()+", DEF:"+defender.getDef()+", DodgeChance:"+defender.getDodgeChance()+", INI:"+defender.getInitiative()+", CritChance:"+defender.getCritChance()+") with "+f.format(defender.getHp())+" hp left.");
+        LOG.info("---------------------------------------------------------------------------------------------------");
 
-        // battle until hp runs out
-        while(defender.getHp() >= 0) {
+        // check if defender dodges
+        boolean dodged = Math.random() < defender.getDodgeChance();
+        LOG.info("[COMBAT]\t"+attacker.getName() + " attacks " + defender.getName());
+        if (!dodged) {
+            LOG.info("[DODGE]\t" + defender.getName() + " dodged " + attacker.getName() + "'s attack!");
+        } else {
+            boolean criticalHit = Math.random() < attacker.getCritChance();
+            double damage = 0.0;
 
-            // swap attacker and defender
-            Hero temp = attacker;
-            attacker = defender;
-            defender = temp;
+            if (criticalHit) {
+                damage = attacker.getAtk() - defender.getDef();
+                if (damage > 0) {
+                    defender.setHp(defender.getHp() - damage);
+                    LOG.info("[ATTACK]\t" + attacker.getName() + " deals " + damage + " damage to " + defender.getName() + "!");
+                }
+                else {
+                    LOG.info("[BLOCK]\t" + defender.getName() + " blocked" + attacker.getName() + "'s attack");
+                }
+            } else {
+                damage = (attacker.getAtk() * 2) - defender.getDef();
+                if (damage > 0) {
+                    defender.setHp(defender.getHp() - damage);
+                    LOG.info("[CRITICAL ATTACK]\t" + attacker.getName() + " deals " + damage + " damage to " + defender.getName() + "!");
+                }
+                else {
+                    LOG.info("[BLOCK]\t" + defender.getName() + " blocked" + attacker.getName() + "'s attack");
+                }
+            }
 
-            // duel round
-            round(attacker, defender);
+            // Update fight-stats of involved heroes:
+            addHarmPointsToStats(attacker, defender, damage);
+
+            if (defender.getHp() <= 0)
+            {
+                LOG.info(defender.getName() + " was defeated!");
+                // Update fight-stats of involved heroes (+1 win resp. +1 defeat):
+                addFightResultToStats(attacker, defender);
+            }
         }
-        LOG.info(defender.getName()+" has lost the duel against "+attacker.getName()+".");
-
-        // Update fight-stats of involved heroes (+1 win resp. +1 defeat):
-        addFightResultToStats(attacker, defender);
-
-        // return loser
-        return defender;
+        LOG.info(attacker.getHeroType().toString() + " " + attacker.getName() + " ATK:" + attacker.getAtk() + " HP " + attacker.getHp());
+        LOG.info(defender.getHeroType().toString() + " " + defender.getName() + " ATK:" + defender.getAtk() + " HP " + defender.getHp());
     }
 
-    private void round(Hero attacker, Hero defender) {
-        double defenderHp = defender.getHp();
-
-        // damage is 1/10 of ATK
-        double damage = attacker.getAtk();
-        if (damage < 1) damage++;
-        damage = damage/10.0;
-        LOG.info("Attacking "+attacker.getName()+" caused "+damage+" damage.");
-
-        // defense blocks DEF percent of ATK
-        double defense = defender.getDef();
-        if (defense > 1) defense--;
-        LOG.info("Defending "+defender.getName()+" blocked "+defense+" percent of damage.");
-
-        // harm = damage - defense
-        double harm = damage - (damage * (defense/100));
-        LOG.info("Defending "+defender.getName()+" lost "+f.format(harm)+" of health.");
-
-        // harm is drawn from defenders hp
-        defenderHp -= harm;
-        LOG.info("Defending "+defender.getName()+" has "+f.format(Math.max(0,defenderHp))+" health points left.");
-
-        // Update fight-stats of involved heroes:
-        addHarmPointsToStats(attacker, defender, harm);
-
-        defender.setHp(defenderHp);
-    }
 
     private void resetFightStats(Party party){
         for(int i=0; i < party.getMembers().size(); i++){
