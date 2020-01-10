1d8449025174a0c59ef256ebf7f7cfc29239e5c6
NikCesar <neonsugar420@gmail.com>
2020-01-10 13:03:01 +0100

    Feign client annotation ergänzt

--- equipment/src/main/java/ch/bfh/swos/equipment/EquipmentApplication.java
+++ equipment/src/main/java/ch/bfh/swos/equipment/EquipmentApplication.java
@@ -3,11 +3,14 @@ package ch.bfh.swos.equipment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 // Enables this microservice to register at Eureka-server (== registry-module)
 // Config-data for registering is in Application.properties
 @EnableEurekaClient
+@EnableFeignClients
 public class EquipmentApplication {
 
     public static void main(String[] args) {
